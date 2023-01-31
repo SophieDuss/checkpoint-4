c9a85af1d685a1a08536cdb6296cf46527b9b861
SophieDuss <sophie.dussillol@gmail.com>
2023-01-31 16:18:23 +0100

    setting backend + BDD

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
+const express = require("express");
 
 const app = require("./src/app");
 
-const port = parseInt(process.env.APP_PORT ?? "5000", 10);
+app.use(express.json());
+
+const port = parseInt(process.env.PORT ?? "8001", 10);
 
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
     // eslint-disable-next-line no-restricted-syntax
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on http://localhost:${port}`);
   }
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,45 +1,24 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const cors = require("cors");
 const router = require("./router");
 
 const app = express();
 
-// use some application-level middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3001",
     optionsSuccessStatus: 200,
+    credentials: true,
   })
 );
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
-// Serve the public folder for public resources
-app.use(express.static(path.join(__dirname, "../public")));
+app.use("/api", router);
+app.use("/", (req, res) => {
+  res.send("welcome  to sophie's checkpoint");
+});
 
-// Serve REACT APP
-app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
-
-// API routes
-app.use(router);
-
-// Redirect all requests to the REACT app
-const reactIndexFile = path.join(
-  __dirname,
-  "..",
-  "..",
-  "frontend",
-  "dist",
-  "index.html"
-);
-
-if (fs.existsSync(reactIndexFile)) {
-  app.get("*", (req, res) => {
-    res.sendFile(reactIndexFile);
-  });
-}
-
-// ready to export
 module.exports = app;
